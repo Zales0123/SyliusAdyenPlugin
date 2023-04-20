14abf376c27d7082ed621f51fc369c6e32bc937b
Mateusz Zalewski <zaleslaw@gmail.com>
2023-04-20 10:22:36 +0200

    Log things in AdyenDetailsAction

--- src/Controller/Shop/AdyenDetailsAction.php
+++ src/Controller/Shop/AdyenDetailsAction.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace BitBag\SyliusAdyenPlugin\Controller\Shop;
 
 use BitBag\SyliusAdyenPlugin\Resolver\Payment\PaymentDetailsResolverInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,7 +24,8 @@ class AdyenDetailsAction
     private $paymentDetailsResolver;
 
     public function __construct(
-        PaymentDetailsResolverInterface $paymentDetailsResolver
+        PaymentDetailsResolverInterface $paymentDetailsResolver,
+        private LoggerInterface $logger
     ) {
         $this->paymentDetailsResolver = $paymentDetailsResolver;
     }
@@ -34,8 +36,12 @@ class AdyenDetailsAction
         $referenceId = $request->query->get(self::REFERENCE_ID_KEY);
 
         if (null === $referenceId) {
+            $this->logger->error('Reference ID is missing in AdyenDetailsAction');
+
             return new Response('Reference ID is missing', Response::HTTP_BAD_REQUEST);
         }
+
+        $this->logger->info('Resolving payment for reference ID ' . substr($referenceId, 0, 20));
 
         $payment = $this->paymentDetailsResolver->resolve($code, $referenceId);
 
